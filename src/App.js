cc41be2a219e0aaa8d2f12fe7477b7f720fa8e2c
Natalia Salyaeva <nat.salyaeva@gmail.com>
2021-05-11 09:49:39 +0300

    images added

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
       <h1>My Notes App</h1>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
+        placeholder="Name"
         value={formData.name}
       />
       <input
